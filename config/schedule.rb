ff0f7516229e04d648bf0fbde9b695e67a332479
amingilani <aminshahgilani@gmail.com>
2016-09-25 21:25:43 +0500

    Add autoupdating to the crontab

--- config/schedule.rb
+++ config/schedule.rb
@@ -3,3 +3,7 @@ set :output, '/usr/local/bin/scruffy/logs/cron.log'
 every 30.minutes do
   command '/usr/local/bin/scruffy/scruffy.rb'
 end
+
+every 1.day do
+  command '/usr/bin/git -C /home/gilani/scruffy/ pull'
+end
